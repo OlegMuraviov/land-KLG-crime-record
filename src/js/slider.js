b3ec56881b4c7099f4208440b09099fc863ce7bb
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-10-18 22:25:54 +0300

    kill parcel

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,13 +1,1 @@
-import { tns } from "../../node_modules/tiny-slider";
-var slider = tns({
-  container: ".my-slider",
-  items: 1,
-  slideBy: 1,
-  autoplay: true,
-  speed: 300,
-  mode: "gallery",
-//   fixedWidth: 210,
-  nav: false,
-  controls: false,
-  autoplayButtonOutput: false,
-});
+
