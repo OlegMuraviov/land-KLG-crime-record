3125665c91642d1ed2e92c7ddb45d63d6e0948fd
Oleg Muraviov <o.muraviov2014@gmail.com>
2022-10-19 14:09:46 +0300

    add language swith to burger

--- src/js/localization.js
+++ src/js/localization.js
@@ -1,31 +1,44 @@
-const select = document.querySelector("select");
+const selectMobile = document.querySelector("#selectMobile");
+const selectDesktop = document.querySelector("#selectDesktop");
 const allLang = ["uk", "ru"];
 
-select.addEventListener("change", changeURLLanguage);
+selectMobile.addEventListener("change", (e) => {
+  changeURLLanguage(e);
+});
+selectDesktop.addEventListener("change", (e) => {
+  changeURLLanguage(e);
+});
 
-function changeURLLanguage() {
-  let lang = select.value;
+function changeURLLanguage(e) {
+  let lang = "";
+  if (e.target.id === "selectMobile") {
+    lang = selectMobile.value;
+  }
+  if (e.target.id === "selectDesktop") {
+    lang = selectDesktop.value;
+  }
   location.href = window.location.pathname + "#" + lang;
   location.reload();
 }
 
 function changeLanguage() {
   let hash = window.location.hash;
-  
+
   hash = hash.substring(1);
-  
+
   if (!allLang.includes(hash)) {
     location.href = window.location.pathname + "#uk";
     location.reload();
   }
 
-  select.value = hash;
+  selectMobile.value = hash;
+  selectDesktop.value = hash;
   document.querySelector("title").innerHTML = langArr["unit"][hash];
   for (let key in langArr) {
-    let elem = document.querySelector('.lng-' + key);
+    let elem = document.querySelector(".lng-" + key);
     if (elem) {
       elem.innerHTML = langArr[key][hash];
-      console.log()
+      console.log();
     }
   }
 }
